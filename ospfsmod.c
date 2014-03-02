8bf40ced13b558dc44aecb0acdebe4faf7e0c446
Braden Anderson <bradencanderson@gmail.com>
2014-03-02 15:20:02 -0800

    preliminary ospfs_read implementation

--- ospfsmod.c
+++ ospfsmod.c
@@ -844,7 +844,9 @@ ospfs_read(struct file *filp, char __user *buffer, size_t count, loff_t *f_pos)
 
 	// Make sure we don't read past the end of the file!
 	// Change 'count' so we never read past the end of the file.
-	/* EXERCISE: Your code here */
+	if (*f_pos + count > oi->oi_size) {
+		count = oi->oi_size - *f_pos;
+	}
 
 	// Copy the data to user block by block
 	while (amount < count && retval >= 0) {
@@ -864,9 +866,14 @@ ospfs_read(struct file *filp, char __user *buffer, size_t count, loff_t *f_pos)
 		// Copy data into user space. Return -EFAULT if unable to write
 		// into user space.
 		// Use variable 'n' to track number of bytes moved.
-		/* EXERCISE: Your code here */
-		retval = -EIO; // Replace these lines
-		goto done;
+		n = (count + (*f_pos % OSPFS_BLKSIZE) - amount > OSPFS_BLKSIZE ?
+			OSPFS_BLKSIZE - (*f_pos % OSPFS_BLKSIZE) : count - amount);
+		retval = copy_to_user(buffer,data,n);
+		
+		if (retval < 0) {
+			retval = -EFAULT;
+			goto done;
+		}
 
 		buffer += n;
 		amount += n;
